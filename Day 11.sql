c7abd32e60dc847edf0fe53ddf9ea31dec0e1971
Daniel Hutmacher <daniel@strd.co>
2023-12-11 11:23:19 +0100

    Day 11, speedrun

--- Day 11.sql
+++ Day 11.sql
@@ -198,7 +198,7 @@ INNER JOIN (
     HAVING COUNT(galaxy_no)=0) AS sub ON map.x BETWEEN sub.from_x AND sub.to_x;
 
 
---- Shift empty y columns down:
+--- Shift empty y rows down:
 UPDATE map
 SET map.y=map.y+sub.shift_len
 FROM #map AS map
@@ -210,6 +210,8 @@ INNER JOIN (
     GROUP BY y
     HAVING COUNT(galaxy_no)=0) AS sub ON map.y BETWEEN sub.from_y AND sub.to_y;
 
+
+--- Result:
 SELECT SUM(ABS(a.x-b.x)+ABS(a.y-b.y))
 FROM #map AS a
 INNER JOIN #map AS b ON a.galaxy_no>b.galaxy_no
@@ -224,10 +226,13 @@ WHERE a.galaxy_no IS NOT NULL
 
 --- Part 2:
 
+
+
+--- Introduce a scale variable:
 DECLARE @scale int=1000000;
 
 
-
+--- Start over:
 TRUNCATE TABLE #map;
 
 INSERT INTO #map (x, y, galaxy_no)
@@ -240,7 +245,7 @@ CROSS JOIN GENERATE_SERIES(1, CAST(LEN(@input)/@width AS int), 1) AS y
 CROSS APPLY (VALUES (NULLIF(SUBSTRING(@input, @width*(y.[value]-1)+x.[value], 1), '.'))) AS s(symbol);
 
 
---- Shift empty x columns to the right:
+--- Shift empty x columns to the right (@scale number of columns):
 UPDATE map
 SET map.x=map.x+(@scale-1)*sub.shift_len
 FROM #map AS map
@@ -253,7 +258,7 @@ INNER JOIN (
     HAVING COUNT(galaxy_no)=0) AS sub ON map.x BETWEEN sub.from_x AND sub.to_x;
 
 
---- Shift empty y columns down:
+--- Shift empty y rows down (@scale number of rows):
 UPDATE map
 SET map.y=map.y+(@scale-1)*sub.shift_len
 FROM #map AS map
@@ -265,6 +270,8 @@ INNER JOIN (
     GROUP BY y
     HAVING COUNT(galaxy_no)=0) AS sub ON map.y BETWEEN sub.from_y AND sub.to_y;
 
+
+--- Result:
 SELECT SUM(ABS(a.x-b.x)+ABS(a.y-b.y))
 FROM #map AS a
 INNER JOIN #map AS b ON a.galaxy_no>b.galaxy_no
