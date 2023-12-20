c4ccfa4ec293d4e449c857b46d55cfef710dab99
Daniel Hutmacher <daniel@strd.co>
2023-12-20 14:56:31 +0100

    Day 14

---
+++ Day 14.sql
@@ -0,0 +1,293 @@
+DECLARE @input varchar(max)='...#.O.#.#.O.O....#...O....#..#........OO.O#...O..O#O#.......O.O....O.....#...OO#.O....OOO..OO#..#..
+.##.....#.OOO.OO#...O.......O#..O.##OO.O.......O##....O....#O..O..#.#....#OO..#...O.O#.#...#....#O.O
+O...##.O.O.O.O..#.#.....#O....#..#O..O...O..OO..O...##.......OO#..OOO..#O..#.O#.O......#..#O..O..O.O
+..OO....OOO....O.O..OOO.....O........#...O.O.O.O..O#.O...O..#.#O........O.....#O...##....#...#.#....
+##.O.O#..O..##O.....O.#.....O.O#O.O..#...#...OO.##OOO...OO#...O...O........O.#.O.........#O.#...#.#.
+.#.O.O.....#..O###..O#.#..O....O.O....#...##.O..O.OOO..O.O..O#.O..O....#.#.O..#..##O...O....#O#.....
+.......O...##..O..O##..##...O....###.O.#.#.O......O...O.O......#O.....#..O..O#..##O.O#O##O....O#....
+.#O..#....##.##..O#..#..#..O........OO....#..##.O.#.O.O..OO.O.O#..#O##O......OO...O...OO...#....OO..
+#.....O#......O#.....#..O...#O..OO..OO.#O...O...O.........O...#O...O.....#.....#...#.....O##.O.....O
+OO.O..O...O......#.O......#O....#..#.#...O.O....O.#.O#...O........O......#.#O#.O...O.........#O....O
+O.O..O..#......O.O.OO...OO..##.....OO.....#.O...O.#.#...OO..OO.O.........#O..#.O...O#.#..O..OO.##O..
+..#.......O....#...#....##..O.O......O....O#.O..O..#.#.O#O.O.#...#.##.....O...O.O...O.#...O...O.#...
+.O...#..O.....O.O.....OO.O.O.......O..O..........#...O..O..O......O....#OOO...O#O...#.O#........O...
+.OO#....#.....O...#.###..#..O..O.....#O..#..OO...OOO#.O#..O#O........#..#.O..OOO#O#..O..OO........#.
+..O.....#..#..O.............#..O.O..O#.......#.O.........O.#..OO.O..#..##..O.O#O....OO.O.O.O.....O#.
+........#O..O#OO...#.##O.O....O...O..........##.#O..O......O..O.#...#O.O.OO...#.#OOO.#...O#....#.O.#
+.....O.O...#OO#.OO#......#..O.O..#.OO.......#...#.O.O#.O.#O.O#..#O##..OO...#......#.O....#.....O.#O.
+.O#OOOO...#.#..........O#.#.#.#.O....O..O...O...OOO.O.O.O..OO..O..O#.#...#....#...#O#.OO.O..#.O#....
+OO#OO..#.O.#....#..#.#..#O#.O#O#.#..O.O....#..O...OO..O..#........#.O...O#.#.....#....O.....#.O.....
+.O.....#...OO...#..O..#..O..O##..OO...O....#.O.#............###.O.#..O..O.........O..#.O..O.#OO.#...
+.##O.#.O.#....O..O..#..O.#.O..##O..O......OO...#...O..OO#....O..O#O.##.OO#...O#...#.......O.O#.#OO..
+.OO#O#...O..##.O......O..O.O....O...OOO.O..#...O#.#.O.OOO..#...#.##..OO..................OOOOO...###
+.#..##...####.O#...O........O...#O..O..O....O##O..O..OO..#.....#...O.....OO......OOO.#..O.##OO##...#
+....O#........O##.O...O#..#.O#O...O...O.O........#..#.#.O......O..#.....#.O..#.#..#..#O....O##...#..
+.O..OO.O##..O...O.OO....O.OOO..#O#.O##......O.O#....O#O.O......#.O..#O..#O.........#..OO....#.OO..O.
+.#OO..#OO.###........#...#..#.O..#O.O#O...O.#.....O...#......O.......#.O.#.##.O..#O.O###OOOO...#..O.
+##...OOO.#..........O#...O#.....OO...O#..OO.OO.#.OOO..OO....O..OOO...##O...O.........##O...#.#O.....
+.#....O.....#OO.O.OO..O......#.OO..#O...O..##O.#O....#O.OO..#O.OO...O..O.#..##...O......#O##OO.#.O..
+.#..O....O....#......#.O.##...OO.O.O...O.OO.O....#...#..O....O#O..#...#.#..#...OO......OOO...O..#...
+#.#OO.#....O.#.OO....#........O.O.##...O.#.................OOOO....O..#......O..#.#...O....#.O#OO.#O
+....O.O.......##..O.........OO#O..O.O#.#.#.O....#..OO.O..O#O....O..O.O......O.O.#..O.......O.O..O.O#
+...........#OO...O....#.O.#..#O.O#.....O..O.....O#.....OO.#O...#O.#O.#.....OO.O.O....OO......O#O.O..
+OOOO.OO..O....O.O...O..#.........O#.##OOO#.O..O.O.O......O..#..O..#..#.O..O....O#O.#OO..O.O...O#.##.
+##.O.#O.#OO#..........OO#.OO.#..#.#......#.OO...#.O.O.......O##....O..O..#.#.#O....O..##.#OO.O..O..O
+#O...OO..#.#....##.O#.#...##OO#........O#.O..O#.........O.O.#....O.#.O.......O..OOO......O#O.###.O..
+....O#....#..O...#.#.....O.O#O..#O....O...O.OO#...O..O#.##.O.....O...#..#.O.....O...O#..O.#...#.#O..
+O.OO......#..O...O.O....#...OO..O#.O..OO..#.##.....#....O.O#O#.O....O..#..OOO..O....O.O..#..O.O..O..
+.#..#..##O..O.....O#.#.O.O..O..OO..O.O..OO.##.O...O..O#........O.............#.OO..#......O..#O#OOO.
+.#..##...O.#..OO...O..#..#O#..#..##.O#.#.....#....O.O..O.........#.###...#.O##..O...#.O.OO..........
+........#O.......O...O#.#....#.O#....#..O.....OO........O...#.........##.....O...O#.O...O.O..#OO.O.#
+.#..#.O##..##.O#.#.......OO...OO#...#O#............#.#...#...O...#.O..#O.#.O.........O..OO.O...O.#..
+#.O#OO...OO.O.#...##........#....O.#.O.....##.#.O.O.O.#..O#...O..#......#..O...###O..OO..##..OO.#OOO
+.OO...#O....O##.O...O.#....O...#O.......O.....OO.O...O..#...OO.OO.O#...O##.....O..#.#...#OO......O..
+..O..#...O...##..O...O..OO.O.O.O.....#.....O.....O..#OOO.....OOOO#.#.O..O..#........O.#O#.#O..O.##..
+.....##.....O.....O#...##.OOO....O.O..O.#.#O........OO.....#...##...#.#......#....OO.O.....#O....#.#
+..O.OO#....O..O.#O.#OO#..#.O.##.#..#.OO##....O#...#..O.#..#...#.#....OOOO.#...OOOO..O.........##O.O.
+O.O.#......O#...O..#.......#O.O..OOOO..#....#.#.OO.O..#O......#.#....#...#..O....#O.OO..O.......#.#.
+...O.O....O#......#.O.O....O#.#..#O.#.....#...#...#.O#.........O.OO..O###...O....O.#.O.#.....##..O..
+.#.OO...O#O.....O#....O......OO...OO.OOOOOO##....#..#..#O.#.OO....O#..OO.#O........O#..O.O#.#.......
+#OO#.#.....O#OOO#...O.........#......O#.......O.....O#.#O...O........#....O...O.....#.......##.OOO#.
+..#.O##..#OO..#....#O....#....#..........#O...#...O.#.O.O###O..O..#...#.OO....##..O.O.O.....O..O....
+....OOO#.#......#..O.O......##O.#.O.#.O.OO....#..O.......#.....#O.O...##O###.##.##..#.O.#.O#..#..O#.
+......O#OO......#.#..#.O#OO#O..O#O.O##.......O...O........O......O#.....#..#....#...#.#..O...O##.#O.
+#..OO..O.O.....#O.O.#.#......O......O.#....O.#OO.........#.O.O#..#..#.O..O.#..#...#...O.#..........O
+..O#OOO#.#.....O.....O..##.#..OOO.O..#..O#...##O..OO...#....#..#...#...OO.....##OO#.OO...#.O....#.#.
+#.#O.............#....O#.....O.....O.##O.....#.#.#.....O....O...O...##..O...#........#O..##.#.....##
+.O.....O...#O.#.O#..O#........#.##...O##O..........O##.O......O..#....##....###......O##..##O..#.O#.
+.#...#..#..O.O.............#....O.OO.....OO#O.....OO..OOO#O.O.O.O...#...#.........OO..O#.O...#.##..#
+..O...#O#.......#OO..........OO#..#.....OO#...#OO.O#....OO....O..O..#....#OO......O#O...#......O..O.
+O.......O..O..#O.....O#O.........O##.O#.#.O.O....##....O.#....O....OOO..#O#...#...O.O#.O.....O.O#.O.
+O.#O....O..##....O.........##.OO###....O...O...O..O#O#...#O.O.O....#..O..#..#.O.......###..#....#..O
+.........O..#O...O..OO.....#...O..#O#.....O....#O#....##...#.OOOO.O..O#...O..O.O#.#..O.O..#..#.OO...
+...#.....#..OO.O.#.#.O.....O#...#..O...O..O..#...O..O.O..#...#.###..#..##O.#.#O.......O..##O.O.O#...
+.##O......O....#....O....#O..#.....#.O..O.O.O..#.#O.....O#.O...#.#O.#...#.........O....O....OO..O...
+.OO.O.O.O#O..O##.O...OO.....O..O....O....#..#...O.O...O....O.#.O......#O.#.O#O..O...O.O.#.O..O.#....
+O#..#.#..#..#...#.O...O.###.##.OO.O.#.OOO..O..#O.#.O...OO...O.OOO....#.....#OO.O##.O.#.#O...#.#.....
+.#.....#.........O.#.........#...OO..O.#.O...O##O...#..O.O.OO#....###.OO.....#..........#.#OO...O.O.
+........OO..O.O.#..O.#.......#.#O.OOO...OO.O............O#O#...O...#.O.....#...........O...#..OO.O#O
+..#.#.#...O....#O.......#...#...O###.O.....O.O.OO....#O...OO.O.O........O.O.#O.O....OO.....OO.....#.
+#..OOO#......O.#.O#...#..#..O..O.O.O.#...O#OOO.#...O.#.OO.#O..#....#..#O....O.#.O...O...O#OO...#....
+.....#.....##...O....#..O..O.....O#OO.#O#.....O...O.##.#........O..O........#.#......##......#..#..O
+O..O....O.##..#.#O##.#.....OO....O.##O......#...#..OO.O.....#...#........O..O#O##.O.......#..##O..##
+#.....#O....O...#OOO#..O#.O.....O..OO.#..#.#.O#..O..O#.O###....O......O.#.O..#O.....#.......##O.O..O
+O.O.#..#....O....O.......#.O..O#O#.O..........#O.....#OO.O...#.OO.....#...O....OO....#...O....O.#..O
+..#...OO....OO......O....#..#..........##..##..#.....##...#..#O.....#OO......#........O.....O..O...#
+.OO#...O#.O....#.........OO#...O#..O.#..O.O.O.#O......##.O.......O...O...###.O......O....O...#.OO#..
+..O..O#O.#..O.#O...OOO#O.#.O....O.#......O....#....###....OO......O...#...OO.#....O........O#..#..OO
+#..OO..O......#...##..O#.O........O.O...##.#O...O...#O#.O.O....#.............#.O#.#..O..#..OO#.....#
+#..........O...O##.O#..O..OO..O.....#O.#.O......#......O.O....##.O#OO..O.O..O.O.O..#..O....O..#....O
+.O.O...O.#..O.O....#.#.O.O.##.##..O..#OOOO.###...#O...#...O.....O...#.##.OO..O.#..#.O.#.......O....#
+#....#....OOOOOOO.#...OO....##O.#..#......O..#..........O......##..O..###..#.O....O.......OO......#.
+.O.O..##.O.#O.#...#....O..O.....#..##......O.......O..O#.O##.#........O..#.O.#.O.O..##..###..##.#...
+..OOO..O##..OOOO.O.....OO##..O..O.#....#.....OO......O.OOO#...#O.OO...O.O......O#..O.##....###...#.O
+....O...O.O....#..O#.O.....O.O.#.......O##...O#..OO.O.O##...#.....O#.###....O....#O.........OO..O.O.
+O#.O......##..#.#..............#......#.#.OO#......##O#OO..OOO..##.O##....#..O...#......##O.O##...OO
+...#OO....OO...O#.#..##....OO.#..#O......O#....#.O.....O.#O..OO.#..OO...O#.O#....O.#.OO.O.O#..O.....
+#..O.OO#O...O..O..........#..#.#..#..#O.....#O#..O....#..O...O.#.#O#.#OO.....OOO.#..#.........O...O#
+....O......#..........O...#.....OOOO........#.#OO.O............O...#....O........#....#....#........
+...O.......###...O.O...#O.##O.OOO.........O.###.O.......O....O.O...O.#.....#..#O..#..OO..O.O...O....
+O.O#....##.O.O.....O....#............OO.#O...O..#...##.#.#O#O.#.....O......O....#.OO#.O#....#.....O.
+..O####.O.........O...........OO..OO.#.....O.......#..O#.O....O#...##.......#.#....O..O..O#..##.....
+.#..#......O.##.O.OOO.O.OO....O.......#......#.O...OOOO...O..#..#.O.#......O...O#.#.O.#.O....O....O.
+##....O.O..O..O.#...O##.......O....#...#OOO.O..##.....#....O.......#O..#.#O..O.#.......O..#.O.O#.O##
+...O....O#O.#...#......#..O.O.O.O...O.....O..O.#..#.....O#...OO...#.O.O....#..OOO...#.....OO.OO..O..
+O##.O.##.O#OO...OO...OO..O....OO#O.#O..O.O....OO..........O..#.O.#O#.......O.O..#O#....#..#...#O..#.
+.#.#..........#.......#..OO#....O.#.#.O.....OO.#.O..#.#.O....#..O....#..#..O.#O#O.....#.O.O.#O.O.#O.
+.O.#....##O...#..OOO.#......#...#.O.......OO....#.......#..#.OOO........OO..O..###O##..O.....O##....
+OO##..O#....O.##.......O...#........O.O...#O#.#.OOO....#......#.#O.O.O..O..O...#.#.O.O.OOO.O#O......
+#O...#....OOO.O...#O....O...O..........O..#..O#.O..#O.....#......#.#..#.OO..#....#O#....O...O....##.
+.OOOO......##.#O.#O...#......O..#..O..#.O...O.OO...............#O...O..##..#.#....O...O..O.##..O.O..';
+
+
+
+
+
+/*
+SET @input='O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....';
+*/
+
+
+DECLARE @canvas TABLE (
+    x               tinyint NOT NULL,
+    y               tinyint NOT NULL,
+    [state]         char(1) NOT NULL,
+    id              smallint NULL,
+    PRIMARY KEY CLUSTERED (y, x),
+    UNIQUE ([state], id, x, y)
+);
+
+--- Populate the canvas:
+
+INSERT INTO @canvas (x, y, [state], id)
+SELECT x.[value] AS x,
+       MAX(y) OVER ()+1-src.y,
+       s.[state],
+       (CASE WHEN s.[state]='O'
+             THEN ROW_NUMBER() OVER (PARTITION BY s.[state] ORDER BY x.[value], src.y) END) AS id
+FROM (
+    SELECT ss.ordinal AS y,
+           ss.[value]
+    FROM STRING_SPLIT(REPLACE(@input, CHAR(13), ''), CHAR(10), 1) AS ss
+    ) AS src
+CROSS APPLY GENERATE_SERIES(1, CAST(LEN(src.[value]) AS int), 1) AS x
+CROSS APPLY (VALUES (SUBSTRING(src.[value], x.[value], 1))) AS s([state]);
+
+--- Add #s along the perimiter:
+INSERT INTO @canvas (x, y, [state])
+SELECT x, MAX(y)+1 AS y, '#' FROM @canvas GROUP BY x
+UNION ALL
+SELECT x, MIN(y)-1 AS y, '#' FROM @canvas GROUP BY x
+UNION ALL
+SELECT MAX(x)+1, y, '#' FROM @canvas GROUP BY y
+UNION ALL
+SELECT MIN(x)-1, y, '#' FROM @canvas GROUP BY y;
+
+DELETE FROM @canvas WHERE [state]='.';
+
+
+
+
+
+
+--- Part 1:
+
+SELECT SUM(y)
+FROM (
+    --- Oooh, this is a variation of the classic gaps-and-islands solution!
+    SELECT _group-ROW_NUMBER() OVER (PARTITION BY x, _group ORDER BY y DESC) AS y
+    FROM (
+        SELECT *, MIN((CASE WHEN [state]='#' THEN y END)) OVER (PARTITION BY x ORDER BY y DESC ROWS UNBOUNDED PRECEDING) AS _group
+        FROM @canvas
+    ) AS sub
+    WHERE [state]='O'
+) AS sub;
+
+
+
+
+
+
+
+
+
+
+
+
+
+--- Part 2:
+
+DECLARE @weights TABLE (
+    iteration       int NOT NULL,
+    [weight]        int NOT NULL,
+    PRIMARY KEY CLUSTERED ([weight], iteration)
+);
+
+DECLARE @start_weight   int=(SELECT SUM(y) FROM @canvas WHERE id IS NOT NULL),
+        @weight         int,
+        @cycle_count    int=0,
+        @done           bit=0,
+        @first_recurrence int,
+        @mod            int,
+        @dt             datetime2(7)=SYSDATETIME();
+
+SET NOCOUNT ON;
+SET ANSI_WARNINGS OFF;  --- The aggregate NULL warnings get a little annoying.
+
+--- With enough iterations, we're going to repeat the same patterns. We'll look use the number of iterations as
+--- a modulus, to compute what the numbers would be at 1 billion iterations.
+WHILE (@done=0) BEGIN;
+
+    --- Tilt the board:
+    WITH cte AS (
+        SELECT id,
+               --- Depending on the direction (@cycle_count%4), update the x and y values:
+               CAST((CASE @cycle_count%4
+                     WHEN 1 THEN _west_group+ROW_NUMBER() OVER (PARTITION BY y, _west_group ORDER BY x)
+                     WHEN 3 THEN _east_group-ROW_NUMBER() OVER (PARTITION BY y, _east_group ORDER BY x DESC)
+                     ELSE x END) AS tinyint) AS x,
+               CAST((CASE @cycle_count%4
+                     WHEN 0 THEN _north_group-ROW_NUMBER() OVER (PARTITION BY x, _north_group ORDER BY y DESC)
+                     WHEN 2 THEN _south_group+ROW_NUMBER() OVER (PARTITION BY x, _south_group ORDER BY y)
+                     ELSE y END) AS tinyint) AS y
+        FROM (
+            SELECT id, [state], x, y,
+                   --- "Groups" are actually stops, in the form of "#" symbols.
+                   MIN((CASE WHEN [state]='#' THEN y END)) OVER (PARTITION BY x ORDER BY y DESC ROWS UNBOUNDED PRECEDING) AS _north_group,
+                   MAX((CASE WHEN [state]='#' THEN x END)) OVER (PARTITION BY y ORDER BY x ROWS UNBOUNDED PRECEDING) AS _west_group,
+                   MAX((CASE WHEN [state]='#' THEN y END)) OVER (PARTITION BY x ORDER BY y ROWS UNBOUNDED PRECEDING) AS _south_group,
+                   MIN((CASE WHEN [state]='#' THEN x END)) OVER (PARTITION BY y ORDER BY x DESC ROWS UNBOUNDED PRECEDING) AS _east_group
+            FROM @canvas
+        ) AS sub
+        --- Index help:
+        WHERE id IS NOT NULL
+          AND [state]='O')
+
+    UPDATE c
+    SET c.x=cte.x, c.y=cte.y
+    FROM @canvas AS c
+    INNER JOIN cte ON c.id=cte.id;
+
+    SET @cycle_count=@cycle_count+1;
+
+    IF (@cycle_count%1000=0)
+        PRINT STR(@cycle_count/4, 10, 0)+' iterations:'+STR(0.001*DATEDIFF(ms, @dt, SYSDATETIME()), 10, 2)+' s.';
+
+    --- Every four cycles (one iteration)...
+    IF (@cycle_count%4=0) BEGIN;
+
+        --- Compute the weight for this run.
+        SELECT @weight=SUM(y) FROM @canvas WHERE id IS NOT NULL;
+
+        --- Save the weight for this iteration.
+        INSERT INTO @weights (iteration, [weight])
+        VALUES (@cycle_count/4, @weight);
+
+        --- If we see that the weight recurs at even intervals...
+        SELECT TOP (1) @first_recurrence=MIN(iteration), @mod=MIN(_lag)+1
+        FROM (
+            SELECT iteration, iteration-LAG(iteration, 1) OVER (PARTITION BY [weight] ORDER BY iteration) AS _lag
+            FROM @weights
+            WHERE [weight]=@weight
+                  --- Give the board time to settle into a recurring pattern before lookig for :
+              AND iteration>100
+        ) AS w
+        WHERE _lag>1
+        HAVING COUNT(DISTINCT _lag)=1
+               --- How many repetitions we want to be sure it's not just a fluke.
+           AND COUNT(*)=5;
+
+        --- ... we've found what we're looking for:
+        IF (@first_recurrence IS NOT NULL) SET @done=1;
+    END;
+
+    --- ... But don't go on forever.
+    IF (@cycle_count>10000)
+        THROW 50001, 'That didn''t pan out.', 1;
+END;
+
+
+
+--- Sometimes, some of the weights can recur with a 6+7+6+7... pattern, so if
+--- we look for the largest modulus (irrespective of which weight it belongs to),
+--- I think we'll be ok.
+SELECT @mod=MAX(_lag) FROM (
+    SELECT iteration-LAG(iteration, 1) OVER (PARTITION BY [weight] ORDER BY iteration) AS _lag
+    FROM @weights
+    WHERE iteration>=@first_recurrence
+) AS sub;
+
+
+--- And here's the answer:
+SELECT [weight]
+FROM @weights
+WHERE iteration=(1000000000-@first_recurrence)%@mod+@first_recurrence;
